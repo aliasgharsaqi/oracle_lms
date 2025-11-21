7e949738d87e215b9aa7edc29065077f16005242
ali-asghar-dev <ali.asghar@aiivatech.com>
2025-11-21 15:37:24 +0500

    updation in to the migrations

--- database/migrations/2025_11_18_120643_add_checkin_checkout_to_student_attendances_table.php
+++ database/migrations/2025_11_18_120643_add_checkin_checkout_to_student_attendances_table.php
@@ -12,16 +12,13 @@ return new class extends Migration
         Schema::table('student_attendances', function (Blueprint $table) {
             $table->timestamp('check_in')->nullable()->after('status');
             $table->timestamp('check_out')->nullable()->after('check_in');
-            // Add leave status columns to match the teacher attendance
-            $table->string('leave_type')->nullable()->after('check_out');
-            $table->string('leave_status')->nullable()->after('leave_type');
         });
     }
 
     public function down()
     {
         Schema::table('student_attendances', function (Blueprint $table) {
-            $table->dropColumn(['check_in', 'check_out', 'leave_type', 'leave_status']);
+            $table->dropColumn(['check_in', 'check_out']);
         });
     }
 };
